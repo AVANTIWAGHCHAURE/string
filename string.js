d1e260f379c4c6ecdedcc0d8eaa2c877b5b9547a
AVANTIWAGHCHAURE <avantiwaghchaure@gmail.com>
2021-10-10 21:28:35 +0530

    first commit

---
+++ string.js
@@ -0,0 +1,73 @@
+// let a = "india"
+// console.log(a)
+// console.log(typeof a)
+
+// console.log("India is my country")
+// console.log('"India" is my country')
+// console.log('India is my country')
+// console.log("India's population is very high")
+// console.log("He is abhi's brother")
+
+// //string literals
+// let name = "kiran"
+// let age = 25
+// console.log(`My name is ${name} and my age is ${age}`)
+
+// let river = "ganga"
+// let length = 4500
+// console.log(`${river} is longest river. It's length is ${length} km`)
+
+// let Name = "kush"
+// let height = 5
+// console.log(`His name is ${Name}. Kush's height is ${height}`)
+
+// //Important Note--------------------------
+// //string + string = string
+// //string + number = string
+// //number + number = number
+// //number + string = string
+
+// let aa = 51
+ let bb = "welcome"
+// let cc = 15
+// console.log(aa+bb)
+// console.log(bb+cc)
+// console.log(cc+aa)
+
+// for(let i = 0; i<bb.length;i++){
+// //console.log(i)
+// console.log (bb[i])
+// }
+
+// for(let i = bb.length-1;i >=0 ;i--){
+//     console.log(bb[i])
+// }
+
+// let dd = "India"
+// let ee = ""
+// // for(let i = 0; i<dd.length;i++){
+// //     ee = dd[i] + ee
+// // }
+// // console.log(ee)
+
+// for(let i = 0;i<dd.length; i++){
+//     ee = ee + dd[i]
+// }
+// console.log(ee)
+// //-------------------------------------->
+
+let ff = "shree"
+let gg = ""
+for(let i = 0;i<ff.length;i++){
+    console.log(ff[i])
+}
+
+for(let i = 0 ;i<ff.length;i++){
+gg = ff[i] + gg
+}
+console.log(gg)
+
+for (let i = 0; i<ff.length;i++){
+    gg = gg + ff[i]
+}
+console.log(gg)
